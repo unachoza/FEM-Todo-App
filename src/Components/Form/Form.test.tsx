4491a5ba1d751483892144813651648c37c6a6e3
arianna choza <unachoza@gmail.com>
2025-01-10 16:33:53 -0500

    added basic test to form

--- src/Components/Form/Form.test.tsx
+++ src/Components/Form/Form.test.tsx
@@ -2,6 +2,15 @@ import { render, screen, userEvent } from "../../utils/test-utils";
 import Form from "./Form";
 
 describe("Form Components", () => {
+	const mockProps = {
+		handleSubmit: vi.fn(),
+		handleChange: vi.fn(),
+		input: "test",
+	};
+	it("renders initial todos correctly", () => {
+		render(<Form {...mockProps} />);
+		expect(screen.getByRole("textbox")).toBeInTheDocument();
+	});
 	it("renders on top of screen ", () => {});
 	it("on enter adds item to todolist ", () => {});
 	it("if user hits enter with nothing in input, it does not add empty todo ", () => {});
