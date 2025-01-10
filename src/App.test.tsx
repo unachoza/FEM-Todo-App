b211a4fbf4cdd10115bee7d50ce3ae56b62cb659
arianna choza <unachoza@gmail.com>
2025-01-10 16:37:56 -0500

    removed unused code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
 import Card from "./Components/Card/Card";
