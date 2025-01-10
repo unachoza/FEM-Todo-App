246d1a1f56199c5b2e94253f8f89e2cddc4adc18
arianna choza <unachoza@gmail.com>
2025-01-10 16:11:29 -0500

    cleaned up tests, 2 failing App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,27 +1,45 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
-import { mockTodos, mockTodoFunctions } from "./mocks/mockTodos";
+import Card from "./Components/Card/Card";
+import { mockTodos } from "./mocks/mockTodos";
 import App from "./App";
 
 describe("App Component", () => {
-
 	beforeEach(() => {
 		vi.clearAllMocks();
-	  });
-
-	 it("should render initial todos", () => {
-		const { container } = render(<App />);
-		expect(container.querySelectorAll('.list-item')).toHaveLength(0);
-		
-	})
-
-});
-
-describe("the app", () => {
-	it("should Todo Test", () => {
-		render(<App />);
-		const message = screen.getByText("Todo");
-		expect(message).toBeVisible();
 	});
+
+	it("renders initial todos correctly", () => {
+		render(<App />);
+		mockTodos.forEach((todo) => {
+			render(<Card {...todo} />);
+		});
+		const checkboxes = screen.getAllByRole("check-item");
+		expect(checkboxes).toHaveLength(3);
+	});
+
+	it("handles todo deletion", () => {
+		render(<App />);
+		mockTodos.forEach((todo) => {
+			render(<Card {...todo} />);
+		});
+
+		const initialTodos = screen.getAllByRole("check-item");
+		const deleteButtons = screen.getAllByRole("button", { name: "cross" });
+
+		fireEvent.click(deleteButtons[0]);
+		const remainingTodos = screen.getAllByRole("check-item");
+		expect(remainingTodos.length).toBe(initialTodos.length - 1);
+	});
+
+	it("handles todo completion toggle", () => {
+		render(<App />);
+		
+		const checkboxes = screen.getAllByRole("checkbox");
+		fireEvent.click(checkboxes[0]);
+		
+		const labels = screen.getAllByRole("check-item");
+		expect(labels[0]).toHaveStyle({ textDecoration: "line-through" });
+	  });
 });
--- src/mocks/mockTodos.ts
+++ src/mocks/mockTodos.ts
@@ -3,16 +3,22 @@ export const mockTodos = [
 		id: "123",
 		title: "Test Todo 1",
 		completed: false,
+		toggle: vi.fn(),
+		deleteTodo: vi.fn(),
 	},
 	{
 		id: "1234",
 		title: "Test Todo 2",
 		completed: true,
+		toggle: vi.fn(),
+		deleteTodo: vi.fn(),
 	},
 	{
 		id: "1236",
 		title: "Test Todo 3",
-		completed: false,
+		completed: true,
+		toggle: vi.fn(),
+		deleteTodo: vi.fn(),
 	},
 ];
 
