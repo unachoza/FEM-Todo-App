5aedd815e98c37cf26c871aad722e00f45d504ae
arianna choza <unachoza@gmail.com>
2025-01-10 16:36:23 -0500

    removed unused code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -18,28 +18,4 @@ describe("App Component", () => {
 		const checkboxes = screen.getAllByRole("check-item");
 		expect(checkboxes).toHaveLength(3);
 	});
-
-	it("handles todo deletion", () => {
-		render(<App />);
-		mockTodos.forEach((todo) => {
-			render(<Card {...todo} />);
-		});
-
-		const initialTodos = screen.getAllByRole("check-item");
-		const deleteButtons = screen.getAllByRole("button", { name: "cross" });
-
-		fireEvent.click(deleteButtons[0]);
-		const remainingTodos = screen.getAllByRole("check-item");
-		expect(remainingTodos.length).toBe(initialTodos.length - 1);
-	});
-
-	it("handles todo completion toggle", () => {
-		render(<App />);
-		
-		const checkboxes = screen.getAllByRole("checkbox");
-		fireEvent.click(checkboxes[0]);
-		
-		const labels = screen.getAllByRole("check-item");
-		expect(labels[0]).toHaveStyle({ textDecoration: "line-through" });
-	  });
 });
--- src/Components/Form/Form.test.tsx
+++ src/Components/Form/Form.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, userEvent } from "../../utils/test-utils";
+import { render, screen } from "../../utils/test-utils";
 import Form from "./Form";
 
 describe("Form Components", () => {
@@ -11,8 +11,4 @@ describe("Form Components", () => {
 		render(<Form {...mockProps} />);
 		expect(screen.getByRole("textbox")).toBeInTheDocument();
 	});
-	it("renders on top of screen ", () => {});
-	it("on enter adds item to todolist ", () => {});
-	it("if user hits enter with nothing in input, it does not add empty todo ", () => {});
-	it("adds new todo to bottom of list", () => {});
 });
